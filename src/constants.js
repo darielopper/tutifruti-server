5c9f33c7d30b8b2697333ff78c23c8c934d31f90
dramosv90 <dariel.ramos.dv@gmail.com>
2020-05-07 22:45:43 -0400

    solve problems related to eslint

--- src/constants.js
+++ src/constants.js
@@ -1,36 +1,37 @@
 module.exports = {
-    messages: {
-        START_BOARD: '$START_BOARD',
-        JOIN_BOARD: '$JOIN_BOARD',
-        LEAVE_BOARD: '$LEAVE',
-        CLOSE_BOARD: '$CLOSE_BOARD',
-        BOARD_NOT_FOUND: '$BOARD_NOT_FOUND',
-        GAME_PAUSED: '$GAME_PAUSED',
-        PAUSE_GAME: '$PAUSE_GAME',
-        GAME_RESUMED: '$GAME_RESUMED',
-        RESUME_GAME: '$RESUME_GAME',
-        INVALID_OPERATION: '$INVALID_OPERATION',
-        BAN_CLIENT: '$BAN',
-        CLIENT_NOT_FOUND: '$USER_NOT_FOUND',
-        CLIENTS: '$CLIENTS',
-        ANSWER: '$ANSWER',
-        SET_TYPE: '$SET_TYPE',
-        INVALID_TYPES: '$INVALID_TYPES',
-        COMMAND_INCORRECT: '$COMMAND_INCORRECT',
-        TYPE: 0,
-        ANSWER: 1
-    },
+  messages: {
+    START_BOARD: '$START_BOARD',
+    JOIN_BOARD: '$JOIN_BOARD',
+    LEAVE_BOARD: '$LEAVE',
+    CLOSE_BOARD: '$CLOSE_BOARD',
+    BOARD_NOT_FOUND: '$BOARD_NOT_FOUND',
+    GAME_PAUSED: '$GAME_PAUSED',
+    PAUSE_GAME: '$PAUSE_GAME',
+    GAME_RESUMED: '$GAME_RESUMED',
+    RESUME_GAME: '$RESUME_GAME',
+    INVALID_OPERATION: '$INVALID_OPERATION',
+    BAN_CLIENT: '$BAN',
+    CLIENT_NOT_FOUND: '$USER_NOT_FOUND',
+    CLIENTS: '$CLIENTS',
+    ANSWER: '$ANSWER',
+    SET_TYPE: '$SET_TYPE',
+    INVALID_TYPES: '$INVALID_TYPES',
+    COMMAND_INCORRECT: '$COMMAND_INCORRECT'
+  },
 
-    errors: {
-        INVALID_OPERATION: 3,
-        CLIENT_NOT_FOUND: 5,
-        INVALID_TYPES: 7,
-    },
+  errors: {
+    INVALID_OPERATION: 3,
+    CLIENT_NOT_FOUND: 5,
+    INVALID_TYPES: 7
+  },
 
-    types: {
-        all: 'fruit,country,name,color,animal,thing,last_name,plant,carreer',
-        simple: 'name,color,country'
-    },
+  types: {
+    all: 'fruit,country,name,color,animal,thing,last_name,plant,carreer',
+    simple: 'name,color,country'
+  },
 
-    gameLetters: 'ABCDEFGHIJQLMNÑOPQRSTUVWXYZ'.split('')
+  gameLetters: 'ABCDEFGHIJQLMNÑOPQRSTUVWXYZ'.split(''),
+
+  TYPE: 0,
+  ANSWER: 1
 }
--- src/server.js
+++ src/server.js
@@ -1,140 +1,140 @@
-const WebSocket = require('ws');
-const utils = require('./utils');
-const boardController = require('./boards');
-const {messages, errors} = require('./constants');
-const Logger = require('./log');
+const WebSocket = require('ws')
+const utils = require('./utils')
+const boardController = require('./boards')
+const { messages, TYPE } = require('./constants')
+const Logger = require('./log')
 
-let ws;
+let ws
 const port = process.env.PORT || 8081
 
 module.exports = {
-    start() {
-        ws = new WebSocket.Server({
-            port
-        });
+  start () {
+    ws = new WebSocket.Server({
+      port
+    })
 
-        ws.on('connection', function open(client) {
-            Logger.info('Connected 1 client');
-            client.send('Connection successfully');
-            Logger.info('Send data');
+    ws.on('connection', function open (client) {
+      Logger.info('Connected 1 client')
+      client.send('Connection successfully')
+      Logger.info('Send data')
 
-            client.on('message', function incomming(message) {
-                Logger.info(message);
+      client.on('message', function incomming (message) {
+        Logger.info(message)
 
-                // Start the Board
-                if (message === messages.START_BOARD) {
-                    const id = utils.uniqueId();
-                    boardController.addClient(id, client);
-                    client.send('BoardId: ' + id);
-                    client.send('ClientId: ' + client.id);
-                    return;
-                }
+        // Start the Board
+        if (message === messages.START_BOARD) {
+          const id = utils.uniqueId()
+          boardController.addClient(id, client)
+          client.send('BoardId: ' + id)
+          client.send('ClientId: ' + client.id)
+          return
+        }
 
-                // Join to a board
-                if (message.startsWith(messages.JOIN_BOARD)) {
-                    const [msg, boardId] = message.split(':');
-                    if (!boardController.addClient(boardId, client, true)) {
-                        client.send(messages.CLOSE_BOARD);
-                    }
-                    client.send('ClientId: ' + client.id);
-                    return;
-                }
+        // Join to a board
+        if (message.startsWith(messages.JOIN_BOARD)) {
+          const boardId = message.split(':').pop()
+          if (!boardController.addClient(boardId, client, true)) {
+            client.send(messages.CLOSE_BOARD)
+          }
+          client.send('ClientId: ' + client.id)
+          return
+        }
 
-                // Pause the Game
-                if (message.startsWith(messages.PAUSE_GAME)) {
-                    const playerForPause = boardController.pauseGame(client.board, client.id);
-                    if (utils.isInvalidClientsResult(playerForPause)) {
-                        client.send(!playerForPause ? messages.BOARD_NOT_FOUND: messages.INVALID_OPERATION);
-                        return;
-                    }
-                    for (clientPlayer of playerForPause) {
-                        clientPlayer.send(messages.GAME_PAUSED);
-                    }
-                    return;
-                }
+        // Pause the Game
+        if (message.startsWith(messages.PAUSE_GAME)) {
+          const playerForPause = boardController.pauseGame(client.board, client.id)
+          if (utils.isInvalidClientsResult(playerForPause)) {
+            client.send(!playerForPause ? messages.BOARD_NOT_FOUND : messages.INVALID_OPERATION)
+            return
+          }
+          for (const clientPlayer of playerForPause) {
+            clientPlayer.send(messages.GAME_PAUSED)
+          }
+          return
+        }
 
-                // Resume the Game
-                if (message.startsWith(messages.RESUME_GAME)) {
-                    const playerForResume = boardController.resumeGame(client.board, client.id);
-                    if (utils.isInvalidClientsResult(playerForResume)) {
-                        client.send(!playerForResume ? messages.BOARD_NOT_FOUND : messages.INVALID_OPERATION);
-                        return;
-                    }
-                    for (clientPlayer of playerForResume) {
-                        clientPlayer.send(messages.GAME_RESUMED);
-                    }
-                    return;
-                }
+        // Resume the Game
+        if (message.startsWith(messages.RESUME_GAME)) {
+          const playerForResume = boardController.resumeGame(client.board, client.id)
+          if (utils.isInvalidClientsResult(playerForResume)) {
+            client.send(!playerForResume ? messages.BOARD_NOT_FOUND : messages.INVALID_OPERATION)
+            return
+          }
+          for (const clientPlayer of playerForResume) {
+            clientPlayer.send(messages.GAME_RESUMED)
+          }
+          return
+        }
 
-                // Ban a Client from the Board
-                if (message.startsWith(messages.BAN_CLIENT)) {
-                    const [msg, clientId] = message.split(':');
-                    const banUser = boardController.banClient(client.board, clientId)
-                    if (banUser) {
-                        [client, banUser].forEach(item => item.send(messages.BAN_CLIENT));
-                        return;
-                    }
-                    client.send(!banUser ? messages.BOARD_NOT_FOUND : messages.CLIENT_NOT_FOUND);
-                    return;
-                }
+        // Ban a Client from the Board
+        if (message.startsWith(messages.BAN_CLIENT)) {
+          const clientId = message.split(':').pop()
+          const banUser = boardController.banClient(client.board, clientId)
+          if (banUser) {
+            [client, banUser].forEach(item => item.send(messages.BAN_CLIENT))
+            return
+          }
+          client.send(!banUser ? messages.BOARD_NOT_FOUND : messages.CLIENT_NOT_FOUND)
+          return
+        }
 
-                // Leave the Board
-                if (message.startsWith(messages.LEAVE_BOARD)) {
-                    const banUser = boardController.banClient(client.board, client.id)
-                    if (banUser) {
-                        const clients = [...boardController.find(client.board).clients];
-                        clients.push(banUser);
-                        clients.forEach(item => item.send(messages.BAN_CLIENT + ': ' + client.id));
-                        return;
-                    }
-                    client.send(!banUser ? messages.BOARD_NOT_FOUND : messages.CLIENT_NOT_FOUND);
-                    return;
-                }
+        // Leave the Board
+        if (message.startsWith(messages.LEAVE_BOARD)) {
+          const banUser = boardController.banClient(client.board, client.id)
+          if (banUser) {
+            const clients = [...boardController.find(client.board).clients]
+            clients.push(banUser)
+            clients.forEach(item => item.send(messages.BAN_CLIENT + ': ' + client.id))
+            return
+          }
+          client.send(!banUser ? messages.BOARD_NOT_FOUND : messages.CLIENT_NOT_FOUND)
+          return
+        }
 
-                // Show all clients connected to the same board
-                if (message.startsWith(messages.CLIENTS)) {
-                    const clients = boardController.getClients(client.board);
-                    if (!clients || !boardController.hasClient(client.board, client.id)) {
-                        client.send(!clients ? messages.BOARD_NOT_FOUND: messages.CLOSE_BOARD);
-                        return;
-                    }
-                    client.send('Clients: ' + clients.join(', '));
-                    return;
-                }
+        // Show all clients connected to the same board
+        if (message.startsWith(messages.CLIENTS)) {
+          const clients = boardController.getClients(client.board)
+          if (!clients || !boardController.hasClient(client.board, client.id)) {
+            client.send(!clients ? messages.BOARD_NOT_FOUND : messages.CLOSE_BOARD)
+            return
+          }
+          client.send('Clients: ' + clients.join(', '))
+          return
+        }
 
-                if (message.startsWith(messages.SET_TYPE)) {
-                    const [msg, boardId, types] = message.split(':');
-                    const validType = boardController.setType(boardId, types);
-                    if (validType !== true) {
-                        client.send(!validType ? messages.BOARD_NOT_FOUND : messages.INVALID_TYPES);
-                        return;
-                    }
-                    client.send(messages.SET_TYPE);
-                    return;
-                }
+        if (message.startsWith(messages.SET_TYPE)) {
+          const [boardId, types] = message.split(':').slice(1)
+          const validType = boardController.setType(boardId, types)
+          if (validType !== true) {
+            client.send(!validType ? messages.BOARD_NOT_FOUND : messages.INVALID_TYPES)
+            return
+          }
+          client.send(messages.SET_TYPE)
+          return
+        }
 
-                if (message.startsWith(messages.ANSWER)) {
-                    const [msg, anwser] = message.split(':');
-                    const answerParts = anwser.split('|');
-                    const validType = boardController.setType(client.board, answerParts[messages.TYPE]);
-                    if (validType !== true) {
-                        client.send(!validType ? messages.BOARD_NOT_FOUND : messages.INVALID_TYPES);
-                        return;
-                    }
-                    client.send(messages.SET_TYPE);
-                    return;
-                }
+        if (message.startsWith(messages.ANSWER)) {
+          const anwser = message.split(':').pop()
+          const answerParts = anwser.split('|')
+          const validType = boardController.setType(client.board, answerParts[TYPE])
+          if (validType !== true) {
+            client.send(!validType ? messages.BOARD_NOT_FOUND : messages.INVALID_TYPES)
+            return
+          }
+          client.send(messages.SET_TYPE)
+          return
+        }
 
-                client.send(message + ': ' + messages.COMMAND_INCORRECT);
-            })
-        });
-    },
+        client.send(message + ': ' + messages.COMMAND_INCORRECT)
+      })
+    })
+  },
 
-    stop() {
-        ws.close();
-    },
+  stop () {
+    ws.close()
+  },
 
-    port() {
-        return port;
-    }
+  port () {
+    return port
+  }
 }
