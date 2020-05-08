0d3a693c5cf089ab52c4d9de864de4052779534a
dramosv90 <dariel.ramos.dv@gmail.com>
2020-05-07 22:14:35 -0400

    fix problem after refactor log file

--- src/log.js
+++ src/log.js
@@ -2,6 +2,6 @@ const env = process.env.NODE_ENV || 'dev';
 
 module.exports = {
     info(message) {
-        (env === 'test') && console.info(message + ' ' + environment);
+        (env !== 'test') && console.info(message);
     }
 }
