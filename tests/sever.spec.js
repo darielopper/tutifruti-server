c5c1a63a739745fe3d48da06cfea0865beae2147
dramosv90 <dariel.ramos.dv@gmail.com>
2020-05-08 00:15:45 -0400

    try to fix

--- tests/sever.spec.js
+++ tests/sever.spec.js
@@ -29,7 +29,7 @@ describe('Websocket server unit tests', () => {
         });
     });
 
-    /*it('Check server show error if try to join wrong board', () => {
+    it('Check server show error if try to join wrong board', () => {
         const messages = [],
             secondMessages = [];
         ws.on('open', () => {
@@ -41,22 +41,29 @@ describe('Websocket server unit tests', () => {
                 expect(messages[0]).to.equal('Connection successfully');
                 return;
             }
-            if (messages.length > 2) {
-                const ws2 = new WebSocket(`ws://localhost:${server.port()}`);
-                ws2.on('message', (data) => {
-                    const boardId = 'wrongID';
-                    ws2.send(`$JOIN_BOARD:${boardId}`);
-                    secondMessages.push(data);
-                    if (secondMessages.length < 2) {
-                        return;
-                    }
+            if (messages.length == 2) {
+                const messageParts = messages[1].split(':');
+                expect(messageParts.length).to.equal(2);
+                expect(messageParts[0]).to.equal('BoardId');
+                expect(messageParts[1].trim()).to.match(/^\w{6,}$/);
+                return;
+            }
+
+            const messageParts = messages[2].split(':');
+            expect(messageParts[0]).to.equal('ClientId');
+            const ws2 = new WebSocket(`ws://localhost:${server.port()}`);
+            ws2.on('message', (data) => {
+                const boardId = 'WrongId';
+                ws2.send(`$JOIN_BOARD:${boardId}`);
+                secondMessages.push(data);
+                if (secondMessages.length > 1) {
                     const lastMessage = secondMessages.slice(-1).pop();
                     expect(lastMessage).to.contain('BOARD_NOT_FOUND');
                     done();
-                });
-            }
+                }
+            });
         })
-    });*/
+    });
 
     it('Check server start board correctly', (done) => {
         const messages = [],
