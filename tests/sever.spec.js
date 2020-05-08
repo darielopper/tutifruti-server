bf79b44da5b418b63b5729ba8d83757e9b5e0cb1
dramosv90 <dariel.ramos.dv@gmail.com>
2020-05-08 19:40:08 -0400

    add test to check error if client try to connect to wrong board

--- tests/sever.spec.js
+++ tests/sever.spec.js
@@ -82,90 +82,20 @@ describe('Websocket server unit tests', () => {
         ws.send('$START_BOARD');
     });
 
-    it.skip('Check server show error if try to join wrong board', () => {
-        /*const messages = [],
-            secondMessages = [];
-        ws.on('open', () => {
-            console.log('here');
-            ws.send('$START_BOARDs');
-        });
-        ws.on('message', (data) => {
-            messages.push(data);
-            if (messages.length == 1) {
-                expect(messages[0]).to.equal('Connection successfully');
-                return;
-            }
-            if (messages.length == 2) {
-                const messageParts = messages[1].split(':');
-                expect(messageParts.length).to.equal(2);
-                expect(messageParts[0]).to.equal('BoardId');
-                expect(messageParts[1].trim()).to.match(/^\w{6,}$/);
-                return;
-            }
-
-            const messageParts = messages[2].split(':');
-            expect(messageParts[0]).to.equal('ClientId');
-            const ws2 = new WebSocket(`ws://localhost:${server.port()}`);
-            ws2.on('message', (data) => {
-                const boardId = 'WrongId';
-                ws2.send(`$JOIN_BOARD:${boardId}`);
-                secondMessages.push(data);
-                if (secondMessages.length > 1) {
-                    const lastMessage = secondMessages.slice(-1).pop();
-                    expect(lastMessage).to.contain('BOARD_NOT_FOUND');
-                    done();
-                }
-            });
-        })*/
-        /*ws.on('open', (done) => {
-            ws.on('message', () => {
-                console.log('here');
-                //done();
-            });
-            ws.send('$START_BOARDs');
-            expect(2).to.eq(23);
-        });*/
+    it('Check server show error if try to join wrong board', () => {
+        const listener = (message) => {
+            const jsonData = JSON.parse(message);
+            expect(!!jsonData).to.true;
+            ws.off('message', listener);
+            ws2.send(`$JOIN_BOARD:wrongBoard`)
+        };
+        const listener2 = (message) => {
+            expect(message).to.contain('BOARD_NOT_FOUND');
+            ws2.off('message', listener2);
+            done();
+        };
+        ws.on('message', listener);
+        ws2.on('message', listener2);
+        ws.send('$START_BOARD');
     });
-
-    it.skip('Check server start board correctly', (done) => {
-        const messages = [],
-            secondMessages = [];
-
-        ws.on('open', () => {
-            console.log('open');
-            ws.send('$START_BOARD');
-        });
-        ws.on('message', (data) => {
-            messages.push(data);
-            if (messages.length == 1) {
-                expect(messages[0]).to.equal('Connection successfully');
-                return;
-            }
-            if (messages.length == 2) {
-                const messageParts = messages[1].split(':');
-                expect(messageParts.length).to.equal(2);
-                expect(messageParts[0]).to.equal('BoardId');
-                expect(messageParts[1].trim()).to.match(/^\w{6,}$/);
-                return;
-            }
-
-            const messageParts = messages[2].split(':');
-            expect(messageParts.length).to.equal(2);
-            expect(messageParts[0]).to.equal('ClientId');
-            expect(messageParts[1].trim()).to.match(/^\w{3,}(-\w{3,}){2,}$/);
-            expect(messages.length).to.equal(3);
-            const ws2 = new WebSocket(`ws://localhost:${server.port()}`);
-            ws2.on('message', (data) => {
-                const boardId = messages[1].split(':').pop().trim();
-                ws2.send(`$JOIN_BOARD:${boardId}`);
-                secondMessages.push(data);
-                if (secondMessages.length > 1) {
-                    const lastMessage = secondMessages.slice(-1).pop();
-                    expect(lastMessage).to.contain('ClientId:');
-                    expect(lastMessage).to.match(/:\s\w{3,}(-\w{3,}){2,}$/);
-                    done();
-                }
-            });
-        })
-    })
 });
